261b3c3536befd04a9f9d5469e1c0ca279b1b81d
Georgi Georgie <Georgi Georgie@DESKTOP-9FI4TKH>
2025-02-28 11:30:43 +0200

    Add project files.

---
+++ ConsoleApp29/Program.cs
@@ -0,0 +1,18 @@
+﻿for (int i = int.Parse(Console.ReadLine());  
+{
+
+        if (i % 2 == 0)
+        {
+
+            Console.WriteLine("Igrachka");
+
+
+        }
+         else
+         {
+
+            Console.WriteLine("Pari");
+    
+         }
+
+}
